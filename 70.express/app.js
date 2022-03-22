96d3836cfb4de5228b7e765182ec6965f2c8da14
Abhinav Singh Bhardwaj <95396382+ab22032002@users.noreply.github.com>
2022-03-22 11:50:20 +0530

    towards addimg the backend but styled instead

--- 70.express/app.js
+++ 70.express/app.js
@@ -3,43 +3,26 @@ const path = require("path");
 // here we're performing the import operation 
 const app = express(); // app created here 
 const port = 80; // i want to run it in porrt 80 
-// serving the static files 
-app.use('/static',express.static('static'));
+ //EXPRESS RALATED STUFF
+app.use('/static',express.static('static'));  // serving the static files 
 
-// this is setting the template engine as pug 
-app.set('view engine','pug');
+/***THIS IS PUG RELATED STUFF  */
+app.set('view engine','pug');                // // this is setting the template engine as pug
+app.set('views',path.join(__dirname,'views')); //setting the views directory 
 
-//setting the views directory 
-app.set('views',path.join(__dirname,'views'));
+// //our pug demo endpoint 
+// app.get('/demo',(req,res)=>{ // handling get request at slash endpoint here only 
+//     res.status(200).render('demo',{ title:'hey joseph', message:'hello there thankyou for teaching me this '});
+// });
+// ENDPOINTS 
+app.get('/',(req,res)=>{
+    const  con = 'use it wisely '
+   // const that = 'this word can be used anywhere so it increases the code reusibility '
+    const params = { 'title': 'vollyball is the best game ', "content" : con};
+    res.status(200).render('index.pug',params);      // render as using the templates 
+})
 
-//our pug demo endpoint 
-app.get('/demo',(req,res)=>{ // handling get request at slash endpoint here only 
-    res.status(200).render('demo',{ title:'hey joseph', message:'hello there thankyou for teaching me this '});
-});
-
-
-app.get("/",(req,res)=>{ // handling get request at slash endpoint here only 
-    res.send("this is my home page first express with harris");
-});
-app.get("/about",(req,res)=>{ // handling get request at /about endpoint here only 
-    res.send("this is my about page first express with harris");
-});
-app.get("/services",(req,res)=>{ // handling get request at /services endpoint here only 
-    res.send("this is my serrvices page with harris");
-});
-app.get("/our_servers",(req,res)=>{
-    res.send("this is my our_servers page with harris");
-});
-app.get("/contact_us",(req,res)=>{ // handling post  request at /contact_us endpoint here only 
-    res.send("this is my contact_uswith harris");
-});
-app.post("/contact_us",(req,res)=>{
-    res.send("this is my first post request is with harris");
-});
-// downstairs will upadate the status also see the indexing which is crucial yet important 
-app.get("/other",(req,res)=>{
-    res.status(404).send("this page is not found ");
-});
+// START THE SERVER 
 app.listen(port,()=>{
     console.log(`the application started successfully running on this  port ${port}`)
 });
