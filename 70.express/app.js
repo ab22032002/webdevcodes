c3db75529a2387c67a1f03e15aa12516241c193d
Abhinav Singh Bhardwaj <95396382+ab22032002@users.noreply.github.com>
2022-03-22 13:14:13 +0530

    added the first backend and submitted the form

--- 70.express/app.js
+++ 70.express/app.js
@@ -1,10 +1,14 @@
+// const { fs } = require("browserify/lib/builtins");
 const express =require("express");
 const path = require("path");
 // here we're performing the import operation 
 const app = express(); // app created here 
 const port = 80; // i want to run it in porrt 80 
+const fs = require("fs");
  //EXPRESS RALATED STUFF
 app.use('/static',express.static('static'));  // serving the static files 
+// using the middleware 
+app.use(express.urlencoded());
 
 /***THIS IS PUG RELATED STUFF  */
 app.set('view engine','pug');                // // this is setting the template engine as pug
@@ -21,7 +25,23 @@ app.get('/',(req,res)=>{
     const params = { 'title': 'vollyball is the best game ', "content" : con};
     res.status(200).render('index.pug',params);      // render as using the templates 
 })
-
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    // the above statement have helped us in collecting the data as output in the console 
+    name1 =req.body.name1
+    age =req.body.age
+    gender= req.body.gender
+    address= req.body.address
+    more=req.body.more
+    //putting into the file
+    let outputwrite = `the name of the client is ${name1},${age} years old ,${gender},reside in ${address} and know about him/her ${more}`
+   // fs.writeFilesync('ouput.txt',outputwrite);
+    fs.writeFileSync('ouput.txt',outputwrite);
+    // console.log(more);
+//   values arrive according to the names rather IDs
+    const params = {'message':'your form have been submitted successfully '}
+    res.status(200).render('index.pug',params);
+})
 // START THE SERVER 
 app.listen(port,()=>{
     console.log(`the application started successfully running on this  port ${port}`)
